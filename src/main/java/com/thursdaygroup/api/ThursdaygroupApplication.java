d9655e339619b83c53e209a9666fb1cf80d6fc68
GuillermoDivan <guillermodivan@hotmail.com>
2023-11-23 09:46:18 -0300

    First commit!

---
+++ src/main/java/com/thursdaygroup/api/ThursdaygroupApplication.java
@@ -0,0 +1,12 @@
+package com.thursdaygroup.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThursdaygroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThursdaygroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thursdaygroup/api/ThursdaygroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thursdaygroup.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThursdaygroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
